2d59b1a1c7393f40c4a5d1cf1d67d3f9f5c6270c
ReginioWarrenjay <159994212+ReginioWarrenjay@users.noreply.github.com>
2024-03-08 11:50:43 +0800

    Create git-python-Reginio.py

---
+++ git-python-Reginio.py
@@ -0,0 +1,9 @@
+# GitActivity-reginio
+print("Warren jay L. Reginio")
+print("Brgy. Sibuyao, Trrijos, Marinduque")
+print("January 01, 2004")
+		
+
+print("Clloge if Information and Computing Sciences")
+print("Bs Information Technology")
+print("2-C")
